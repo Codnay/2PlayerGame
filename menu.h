4cfd713b925e6e1109cdd3989c43f5829eef5076
Tushar2023 <58504958+Tushar2023@users.noreply.github.com>
2021-05-16 02:34:35 +0530

    changed look of the starting of the game

---
+++ menu.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <unistd.h>
+#include "font.h"
+#include "constans.h"
+
+SDL_Surface *background;
+SDL_Texture *background_texture;
+TTF_Font *title;
+
+void server_or_client(SDL_Renderer *renderer, char *menu, TTF_Font *font);
+void ask_for_ip(SDL_Renderer *renderer, TTF_Font *font, char *ip);
+void how_to_play(SDL_Renderer *renderer, char *menu, TTF_Font *font);
+
+#endif
