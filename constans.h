0bdfc7bac99b41fd6b23f44300e4e51d9157e2ff
Tushar2023 <58504958+Tushar2023@users.noreply.github.com>
2021-05-17 02:17:45 +0530

    increased screen size

--- constans.h
+++ constans.h
@@ -3,30 +3,30 @@
 
 #include "stdbool.h"
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 960
 #define FRAME_TIME 16666
-#define TILE_SIZE 32
+#define TILE_SIZE 64
 #define GRAVITY 1
 #define MAX_VERTICAL_SPEED 25
 #define PLAYER_JUMP_POWER 25
-#define PLAYER_WIDTH 16
-#define PLAYER_HEIGHT 16
+#define PLAYER_WIDTH 32
+#define PLAYER_HEIGHT 32
 #define PLAYER_SPEED 4
 #define POWER_PLAYER_SPEED 8
 #define SLOW_PLAYER_SPEED 2
-#define BULLET_HEIGHT 8
-#define BULLET_WIDTH 8
-#define MAX_PLAYERS 10
-#define SPAWN_A 544
-#define SPAWN_B 32
+#define BULLET_HEIGHT 16
+#define BULLET_WIDTH 16
+#define MAX_PLAYERS 2
+#define SPAWN_A 1088
+#define SPAWN_B 64
 #define CLIENT_FRAME_TIME 15
 #define SERVER_FRAME_TIME 15
 #define X_AXIS 1
 #define Y_AXIS 0
 #define BUF_MAX 256
-#define SPAWN_X 32
-#define SPAWN_Y 416
+#define SPAWN_X 64
+#define SPAWN_Y 832
 #define LEFT_KEY 0x01
 #define RIGHT_KEY 0x02
 #define UP_KEY 0x04
@@ -34,10 +34,10 @@
 #define ATTACK_KEY 0x10
 #define POWERA_KEY 0x20
 #define POWERUPA_APPLY_TIME 150
-#define POWERUPB_APPLY_TIME 400
+#define POWERUPB_APPLY_TIME 150
 #define WAIT_FRAME 6
-#define POWERUP_WIDTH 8
-#define POWERUP_HEIGHT 8
+#define POWERUP_WIDTH 32
+#define POWERUP_HEIGHT 32
 #define POWERUP_SPAWNTIME 3000
 #define MAX_POWERUP 8
 
