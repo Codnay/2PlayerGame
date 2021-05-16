4ede7536336429ba50f508150a791c02c853d16d
Tushar2023 <58504958+Tushar2023@users.noreply.github.com>
2021-05-17 02:24:46 +0530

    updated values

--- menu.c
+++ menu.c
@@ -30,10 +30,10 @@ void server_or_client(SDL_Renderer *renderer, char *menu, TTF_Font *font){
         background_texture = SDL_CreateTextureFromSurface(renderer, background);
         SDL_FreeSurface(background);
         SDL_RenderCopy(renderer,background_texture, NULL, NULL);
-        title = TTF_OpenFont("resources/m5x7.ttf", 80);
-        disp_text(renderer, "DeathRace", title, 200, 20);
-        disp_text(renderer, "[s]erver or [c]lient?", font, 240, 250);
-        disp_text(renderer, "[h]ow to play?", font, 280, 400);
+        title = TTF_OpenFont("resources/m5x7.ttf", 140);
+        disp_text(renderer, "DeathRace", title, 450, 40);
+        disp_text(renderer, "[s]erver or [c]lient?", font, 525, 500);
+        disp_text(renderer, "[h]ow to play?", font, 575, 800);
         SDL_RenderPresent(renderer);
     }
 }
@@ -56,16 +56,16 @@ void how_to_play(SDL_Renderer *renderer, char *menu, TTF_Font *font){
         background_texture = SDL_CreateTextureFromSurface(renderer, background);
         SDL_FreeSurface(background);
         SDL_RenderCopy(renderer,background_texture, NULL, NULL);
-        title = TTF_OpenFont("resources/m5x7.ttf", 80);
-        disp_text(renderer, "DeathRace", title, 200, 20);
-        disp_text(renderer, "This is a two player game.", font, 200, 100);
-        disp_text(renderer, "Players have to reach each other bases, whoever", font, 110, 150);
-        disp_text(renderer, "reaches other's base first wins!", font, 170, 200);
-        disp_text(renderer, "Each player has three bullets, fired with Z key, on hit forces ", font, 70, 250);
-        disp_text(renderer, "the opponent to respawn from the starting.", font, 150, 300);
-        disp_text(renderer, "There are some POWERUPS in the path which can increase the player's speed", font, 10, 350);
-        disp_text(renderer, "or freeze the opponent for a short while.", font, 150, 400);
-        disp_text(renderer, "[r]eturn", font, 280, 450);
+        title = TTF_OpenFont("resources/m5x7.ttf", 160);
+        disp_text(renderer, "DeathRace", title, 400, 40);
+        disp_text(renderer, "This is a two player game.", font, 400, 200);
+        disp_text(renderer, "Players have to reach each other bases, whoever", font, 220, 300);
+        disp_text(renderer, "reaches other's base first wins!", font, 340, 400);
+        disp_text(renderer, "Each player has three bullets, fired with Z key, on hit forces ", font, 140, 500);
+        disp_text(renderer, "the opponent to respawn from the starting.", font, 300, 600);
+        disp_text(renderer, "There are some POWERUPS in the path which can increase the player's speed", font, 20, 700);
+        disp_text(renderer, "or freeze the opponent for a short while.", font, 300, 800);
+        disp_text(renderer, "[r]eturn", font, 560, 900);
         SDL_RenderPresent(renderer);
     }
 }
@@ -104,10 +104,10 @@ void ask_for_ip(SDL_Renderer *renderer, TTF_Font *font, char *ip) {
         background_texture = SDL_CreateTextureFromSurface(renderer, background);
         SDL_FreeSurface(background);
         SDL_RenderCopy(renderer,background_texture, NULL, NULL);
-        title = TTF_OpenFont("resources/m5x7.ttf", 80);
-        disp_text(renderer, "DeathRace", title, 200, 20);
-        disp_text(renderer, "ip addres", font, 240, 200);
-        disp_text(renderer, ip, font, 240, 230);
+        title = TTF_OpenFont("resources/m5x7.ttf", 160);
+        disp_text(renderer, "DeathRace", title, 400, 40);
+        disp_text(renderer, "ip addres", font, 480, 400);
+        disp_text(renderer, ip, font, 480, 460);
         SDL_RenderPresent(renderer);
     }
 }
